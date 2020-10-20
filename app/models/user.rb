bc2064fe4def1d1fbe88d9893a688e09a4585b2d
muratamasaya <capriciouscat709@gmail.com>
2020-10-20 19:31:31 +0900

    deviseに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nick_name, presence: true
+         validates :family_name, presence: true
+         validates :first_name, presence: true
+         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/, message: '全角文字を使用してください' } do
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/, message: '全角文字を使用してください' } do
+         validates :birth_day, presence: true
+         end
+        end
 end
+
