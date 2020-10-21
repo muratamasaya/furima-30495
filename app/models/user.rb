d5e58ed0d5f56a322b72d1b59c2b71395e933907
muratamasaya <capriciouscat709@gmail.com>
2020-10-21 19:12:25 +0900

    ユーザー管理機能の実装とテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nick_name, presence: true
+         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '６文字以上の半角英数字を入力してください' }
          validates :family_name, presence: true
          validates :first_name, presence: true
-         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/, message: '全角文字を使用してください' } do
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/, message: '全角文字を使用してください' } do
+         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/ }
          validates :birth_day, presence: true
-         end
-        end
+         
+        
 end
 
