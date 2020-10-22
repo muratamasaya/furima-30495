b2ee2b87d91018575837cbb424d7281cc4353d58
muratamasaya <capriciouscat709@gmail.com>
2020-10-22 17:32:51 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user, optional: true
+
+  with_options presence: true do
+    validates :product
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :price
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :items
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
