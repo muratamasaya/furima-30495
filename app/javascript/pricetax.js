3e84113ce34b6378343d266dd30d372d86fb94ec
muratamasaya <capriciouscat709@gmail.com>
2020-10-25 18:05:37 +0900

    turbolinksの無効化

--- app/javascript/pricetax.js
+++ app/javascript/pricetax.js
@@ -1,12 +1,11 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price")
+window.addEventListener('load', function() {
+const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value
 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = (Math.floor(priceInput.value / 10))
+    addTaxDom.innerHTML = Math.floor(priceInput.value / 10)
 
     const add = document.getElementById("profit");
-    add.innerHTML = (Math.floor(priceInput.value - [priceInput.value / 10]))
+    add.innerHTML = Math.floor(priceInput.value - [priceInput.value / 10])
   })
 })
