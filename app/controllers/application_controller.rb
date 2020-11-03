9c499108379108e3427eff88d65238c6b57ef097
muratamasaya <capriciouscat709@gmail.com>
2020-11-03 17:30:08 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -6,4 +7,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
   end
+
+  def basic_auth
+   authenticate_or_request_with_http_basic do |username, password|
+   username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
+end
 end
